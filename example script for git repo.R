d1e6314c5984cf6c87b34a93d2a7ab476ac4cc21
JarPar01 <jaroslaw.parzych@astrazeneca.com>
2021-10-05 16:51:12 +0200

    Added new script from exercise 2 to repo

---
+++ example script for git repo.R
@@ -0,0 +1,8 @@
+#Create a new R script. Use the iris dataset 
+#(this comes with base R so if you type iris in the console you will see the data)
+#and plot the Sepal.Length vs Sepal.Width.
+
+#Save the script. Make sure it’s in the same folder as the other files for this project.
+#The saved file should appear in the Git tab with yellow squares next to it. 
+#Add and commit the file, then push it to the remote repository. Check that it has appeared in GitHub.
+
